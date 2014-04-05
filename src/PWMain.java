f8ce7902cfb56e55fba1f23afacf508244bfa740
Jrevolution <jeremy.siao@live.com>
2014-04-05 17:37:22 +0800

    Got the main screens and back buttons, etc....

---
+++ src/PWMain.java
@@ -0,0 +1,16 @@
+import com.kilobolt.framework.Screen;
+import com.kilobolt.framework.implementation.AndroidGame;
+
+
+public class PWMain extends AndroidGame{
+
+	@Override
+	public Screen getInitScreen() {
+		return new LoadingScreen(this);
+	}
+	
+	@Override
+	public void onBackPressed() {
+		getCurrentScreen().backButton();
+	}
+}
